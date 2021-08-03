eb71fadddb8754e4ceed7c08345c276c11bb365e
Morre Thierry <thierry.morre@cognitic.be>
2021-08-03 15:46:03 +0200

    Ajoutez des fichiers projet.

---
+++ DemoCheckBoxList/Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using DemoCheckBoxList.Models.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCheckBoxList.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            RegisterForm form = new RegisterForm()
+            {
+                Choices = GetSelectListItems()
+            };
+
+            return View(form);
+        }
+
+        [HttpPost]
+        public IActionResult Index(RegisterForm form, string[] choices)
+        {
+            form.Choices = GetSelectListItems();
+            foreach(SelectListItem selectListItem in form.Choices)
+            {
+                selectListItem.Selected = choices.Contains(selectListItem.Value);
+            }
+
+            return View(form);
+        }
+
+        private SelectListItem[] GetSelectListItems()
+        {
+            return new SelectListItem[]
+            {
+                new SelectListItem("Choice 1", "1"),
+                new SelectListItem("Choice 2", "2"),
+                new SelectListItem("Choice 3", "3"),
+            };
+        }
+    }
+}
---
+++ DemoCheckBoxList/Models/Forms/RegisterForm.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCheckBoxList.Models.Forms
+{
+    public class RegisterForm
+    {
+        public string Name { get; set; }
+
+        public SelectListItem[] Choices { get; set; }
+    }
+}
